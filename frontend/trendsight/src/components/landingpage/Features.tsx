dc14caa23842d70acba264486010d3f8aee90a42
Kaditya67 <322adityakumar0004@dbit.in>
2024-07-29 23:42:32 +0530

    responsive landing page ui

--- frontend/trendsight/src/components/landingpage/Features.tsx
+++ frontend/trendsight/src/components/landingpage/Features.tsx
@@ -1,26 +1,26 @@
 // src/components/Features.tsx
 import React from 'react';
-import Image1 from '../../assets/images/landingpageFeature1.jpg'
-import Image2 from '../../assets/images/landingpageFeature2.jpg'
-import Image3 from '../../assets/images/landingpageFeature3.png'
+import Image1 from '../../assets/images/landingpageFeature1.jpg';
+import Image2 from '../../assets/images/landingpageFeature2.jpg';
+import Image3 from '../../assets/images/landingpageFeature3.png';
 
 const Features: React.FC = () => {
   return (
-    <section id="features" className="py-10 bg-gray-50">    
+    <section id="features" className="py-10 bg-gray-50">
       <div className="container mx-auto px-4">
-        <h2 className="text-center text-3xl font-semibold">Features</h2>
-        <div className="flex flex-wrap mt-6 justify-center space-y-6 md:space-y-0 md:space-x-6">
+        <h2 className="text-center text-3xl font-semibold mb-6">Features</h2>
+        <div className="flex flex-wrap justify-center lg:gap-6">
           {/* Feature Card 1 */}
-          <div className="w-full md:w-1/3 px-4">
-            <div className="card mb-6 bg-white rounded-lg shadow-lg">
+          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-4 mb-6 md:mb-8 lg:mb-10">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
               <img
                 src={Image1}
-                className="card-img-top object-cover h-56 w-full rounded-t-lg"
-                alt="Feature 1"
+                className="object-cover h-56 w-full"
+                alt="Effortless Stock Analysis"
               />
-              <div className="card-body p-4">
-                <h5 className="card-title text-lg font-bold">Effortless Stock Analysis</h5>
-                <p className="card-text mt-2 text-gray-600">
+              <div className="p-4">
+                <h5 className="text-xl font-semibold">Effortless Stock Analysis</h5>
+                <p className="mt-2 text-gray-600">
                   Search for stocks, choose analysis periods, and gain insights through clear
                   visualizations of trends and technical indicators - all within a user-friendly
                   interface.
@@ -30,16 +30,16 @@ const Features: React.FC = () => {
           </div>
 
           {/* Feature Card 2 */}
-          <div className="w-full md:w-1/3 px-4">
-            <div className="card mb-6 bg-white rounded-lg shadow-lg">
+          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-4 mb-6 md:mb-8 lg:mb-10">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
               <img
                 src={Image2}
-                className="card-img-top object-cover h-56 w-full rounded-t-lg"
-                alt="Feature 2"
+                className="object-cover h-56 w-full"
+                alt="Data-Driven Decisions"
               />
-              <div className="card-body p-4">
-                <h5 className="card-title text-lg font-bold">Data-Driven Decisions</h5>
-                <p className="card-text mt-2 text-gray-600">
+              <div className="p-4">
+                <h5 className="text-xl font-semibold">Data-Driven Decisions</h5>
+                <p className="mt-2 text-gray-600">
                   Leverage TrendSight's calculations of technical indicators like EMAs and
                   potentially RSI to understand market behavior and make informed investment
                   choices.
@@ -49,16 +49,16 @@ const Features: React.FC = () => {
           </div>
 
           {/* Feature Card 3 */}
-          <div className="w-full md:w-1/3 px-4">
-            <div className="card mb-6 bg-white rounded-lg shadow-lg">
+          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 px-4 mb-6 md:mb-8 lg:mb-10">
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
               <img
                 src={Image3}
-                className="card-img-top object-cover h-56 w-full rounded-t-lg"
-                alt="Feature 3"
+                className="object-cover h-56 w-full"
+                alt="Customizable Alerts"
               />
-              <div className="card-body p-4">
-                <h5 className="card-title text-lg font-bold">Customizable Alerts</h5>
-                <p className="card-text mt-2 text-gray-600">
+              <div className="p-4">
+                <h5 className="text-xl font-semibold">Customizable Alerts</h5>
+                <p className="mt-2 text-gray-600">
                   Set personalized alerts based on user-defined criteria, empowering users to stay
                   informed about significant market developments and potential investment
                   opportunities.
--- frontend/trendsight/src/components/landingpage/Navbar.tsx
+++ frontend/trendsight/src/components/landingpage/Navbar.tsx
@@ -1,11 +1,17 @@
 // src/components/Navbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/images/TrendSight_logo.png'
+import Logo from '../../assets/images/TrendSight_logo.png';
 
 const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="bg-white border-b border-gray-200">
+    <nav className="bg-white border-b border-gray-200 relative">
       <div className="container mx-auto flex items-center justify-between px-4 py-3 md:py-5">
         <Link className="navbar-brand" to="/">
           <img
@@ -17,7 +23,7 @@ const Navbar: React.FC = () => {
         <button
           className="text-gray-500 hover:text-gray-600 focus:outline-none md:hidden"
           aria-label="Toggle menu"
-          onClick={() => document.getElementById('navbarNav')?.classList.toggle('hidden')}
+          onClick={toggleMenu}
         >
           <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
             <path
@@ -29,18 +35,21 @@ const Navbar: React.FC = () => {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:flex md:items-center md:w-auto" id="navbarNav">
-          <ul className="flex flex-col mt-3 space-y-2 md:flex-row md:space-x-6 md:space-y-0">
+        <div
+          className={`absolute top-16 right-0 bg-white border border-gray-200 shadow-lg md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}
+          id="navbarNav"
+        >
+          <ul className="flex flex-col items-center p-4 space-y-2">
             <li className="nav-item">
               <a className="nav-link text-gray-700 hover:text-gray-900" href="#features">
                 <h6>Features</h6>
               </a>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <a className="nav-link text-gray-700 hover:text-gray-900" href="#pricing">
                 <h6>Pricing</h6>
               </a>
-            </li>
+            </li> */}
             <li className="nav-item">
               <a className="nav-link text-gray-700 hover:text-gray-900" href="#contact">
                 <h6>Contact</h6>
