a5906e1b2c386defb03226add0b69380bdb4467d
UmmeAtiyaa <ummeatiyaquraishi@gmail.com>
2024-08-06 15:46:40 +0530

    corrected error in index

--- frontend/trendsight/src/pages/IndexPage/index.tsx
+++ frontend/trendsight/src/pages/IndexPage/index.tsx
@@ -1,19 +1,19 @@
-import Auth from "../../components/Auth"
+import React from 'react';
+import Auth from '../../components/Auth/Auth';
 import Contact from "../../components/landingpage/Contact"
 import Features from "../../components/landingpage/Features"
 import Hero from "../../components/landingpage/Hero"
 import Navbar from "../../components/landingpage/Navbar"
 
-function IndexPage() {
+const IndexPage: React.FC = () => {
   return (
     <div>
-      <Auth/>
-      {/* <Navbar/>
-      <Hero/>
-      <Features/>
-      <Contact/> */}
+      <Navbar />      
+      <Hero />
+      <Features />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
